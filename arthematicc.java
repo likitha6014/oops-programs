03a0e9cb2cdc1c671a28334c817b541c511d03dd
likitha6014 <likhitharoyal6014@gmail.com>
2025-07-29 17:02:17 +0530

    arthematic

---
+++ arthematicc.java
@@ -0,0 +1,14 @@
+package oopsprograms;
+
+public class arthematicc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub System.out.println(5+2); //7
+	    System.out.println(5-2); //3
+	    System.out.println(5*2); //10
+	    System.out.println(5/2); //2
+	    System.out.println(5%2); //1
+
+	}
+
+}
