b4be43d8385ef9db1132e000c9ba468b81c0e2c9
likitha6014 <likhitharoyal6014@gmail.com>
2025-07-29 17:04:47 +0530

    program5

---
+++ relational.java
@@ -0,0 +1,17 @@
+package oopsprograms;
+
+public class relational {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5<2); 
+  	   System.out.println(5<=2); 
+	   System.out.println(5>2); 
+	   System.out.println(5>=2); 
+	   System.out.println(5==2); 
+	   System.out.println(5!=2); 
+
+
+	}
+
+}
