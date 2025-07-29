934b40b94e6ceeabdf6090c0bb44e731b16ce600
likitha6014 <likhitharoyal6014@gmail.com>
2025-07-29 16:21:53 +0530

    program1

---
+++ inplicit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		byte b = 10;
+		int i;
+		i=b;
+		System.out.println(b);
+		System.out.println(i);
+
+	}
+}
