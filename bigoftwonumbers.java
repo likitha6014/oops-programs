60e8dbd066d5a43be4d7b7cfa6671654f0a8ba8e
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-02 11:13:28 +0530

    program7

---
+++ bigoftwonumbers.java
@@ -0,0 +1,14 @@
+package oopsprograms;
+
+public class bigoftwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=45;
+		int y=54;
+		System.out.println((x>y)?x:y);
+
+
+	}
+
+}
