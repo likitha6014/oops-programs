48b317a9058368b714e3a977fe3f83b01edaf47b
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-02 10:46:54 +0530

    program6

---
+++ evenoddprograms.java
@@ -0,0 +1,11 @@
+package oopsprograms;
+
+public class evenoddprograms {
+
+	public static void main(String[] args) {
+		int x=41;
+		System.out.println((x%2==0)?"even number":"odd number");
+
+	}
+
+}
