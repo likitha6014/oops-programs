59c3b80b0176e1fea3dafabde1f59f9324b02629
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-05 14:32:56 +0530

    program12s

---
+++ example2.java
@@ -0,0 +1,12 @@
+
+		package strings;
+
+		public class example2 {
+
+		    public static void main(String[] args) {
+		        StringBuffer sb = new StringBuffer("Hello ");
+		        sb.insert(1, "Java");
+		        System.out.println(sb);
+		    }
+		}
+	
