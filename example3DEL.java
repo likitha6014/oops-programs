7e52171f4a8a40b32cbc8175c47bf210cb1b8b61
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-05 14:33:26 +0530

    program13s

---
+++ example3DEL.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class example3DEL {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer("Hello ");
+        sb.delete(1, 4);
+        System.out.println(sb);
+    }
+
+	}
+
