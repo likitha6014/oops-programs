0addea7350fccddb95f18064f9105569fb049499
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-05 14:34:53 +0530

    program15s

---
+++ example5REVERSE.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class example5REVERSE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer("likhitha");
+        sb.reverse();
+        System.out.println(sb);
+    }
+	}
+
