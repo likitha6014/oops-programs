a05f60dc9e304a9849b66c6c2d0c3ff87f9477d7
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-05 14:34:10 +0530

    program14s

---
+++ example4replace.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class example4replace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuffer sb = new StringBuffer("Hello ");
+        sb.replace(1,3 , "good");
+        System.out.println(sb);
+    }
+
+	}
+
