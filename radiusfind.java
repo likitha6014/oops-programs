3ebffb9121eb468cf59fd6ebf2756884895b4c48
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-09 11:10:23 +0530

    radius

---
+++ radiusfind.java
@@ -0,0 +1,16 @@
+package oopsprograms;
+
+public class radiusfind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int area;
+	    
+	    int PI=2,radius=2;
+	    
+	       area = radius * radius * PI;
+	      System.out.println("The area for the circle of radius " +radius + " is " + area);
+
+	}
+
+}
