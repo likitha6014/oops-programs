cc4aaf15678773df027f6eb16b49bd6089ecdd1a
likitha6014 <likhitharoyal6014@gmail.com>
2025-08-19 14:47:35 +0530

    program25

---
+++ override.java
@@ -0,0 +1,49 @@
+package polymorphisim;
+class Bank
+{  
+	int getRateOfInterest()
+	{
+	return 0;
+	}  
+}  
+
+class ICICI extends Bank
+{  
+   int getRateOfInterest()
+   {
+     return 7;
+    } 
+  }
+
+class AXIS extends Bank
+{  
+int getRateOfInterest()
+   {
+	return 9;
+    }  
+}
+
+class SBI extends Bank
+{ 
+
+int getRateOfInterest()
+{
+	return 8;
+} 
+} 
+
+public class override {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SBI s=new SBI();  
+		ICICI i=new ICICI();  
+		AXIS a=new AXIS();  
+
+		System.out.println("SBI Rate of Interest: "+s.getRateOfInterest());  
+		System.out.println("ICICI Rate of Interest: "+i.getRateOfInterest());  
+		System.out.println("AXIS Rate of Interest: "+a.getRateOfInterest());
+		 
+	}
+
+}
